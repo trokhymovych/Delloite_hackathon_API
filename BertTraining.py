66de955c0e4c6608aa4e3836a88d44a4ddd77282
Mykola Trokhymovych <mtro@ciklum.com>
2020-06-29 18:37:16 +0300

    Add evaluator

--- BertTraining.py
+++ BertTraining.py
@@ -62,6 +62,19 @@ class BertTraining:
         self.train_data2 = SentencesDataset(self.reader.get_examples(self.df2, modelname='model_2'), model=self.model2)
         self.train_dataloader2 = DataLoader(self.train_data2, shuffle=True, batch_size=self.batch_size)
         Logger.logger.info("[INFO] Second loader initialized")
+
+        # val data
+        fold = int(len(self.df1) * 0.95)
+        dev_data1 = SentencesDataset(self.reader.get_examples(self.df1[fold:], modelname='model_1'), model=self.model1)
+        dev_dataloader1 = DataLoader(dev_data1, shuffle=False, batch_size=self.batch_size)
+        self.evaluator1 = EmbeddingSimilarityEvaluator(dev_dataloader1)
+
+        # val data
+        fold = int(len(self.df2) * 0.95)
+        dev_data2 = SentencesDataset(self.reader.get_examples(self.df2[fold:], modelname='model_2'), model=self.model2)
+        dev_dataloader2 = DataLoader(dev_data2, shuffle=False, batch_size=self.batch_size)
+        self.evaluator2 = EmbeddingSimilarityEvaluator(dev_dataloader2)
+
     def fit(self, ):
         # Configure the training
         num_epochs = 1
@@ -69,7 +82,9 @@ class BertTraining:
             len(self.train_dataloader1) * num_epochs / self.batch_size * 0.1)  # 10% of train data for warm-up
         # Train the model
         self.model1.fit(train_objectives=[(self.train_dataloader1, self.train_loss1)],
-                       epochs=num_epochs,
+                        evaluator=self.evaluator1,
+                        epochs=num_epochs,
+                        evaluation_steps=100,
                        warmup_steps=warmup_steps,
                        output_path=self.model_save_path1
                        )
@@ -79,12 +94,14 @@ class BertTraining:
             len(self.train_dataloader2) * num_epochs / self.batch_size * 0.1)  # 10% of train data for warm-up
 
         self.model2.fit(train_objectives=[(self.train_dataloader2, self.train_loss2)],
-                       epochs=num_epochs,
+                        evaluator=self.evaluator2,
+                        epochs=num_epochs,
+                        evaluation_steps=100,
                        warmup_steps=warmup_steps,
                        output_path=self.model_save_path2
                        )
 
-    def get_embeddings(self, input_f='data/train_only_text.pkl', output_f='train_bert_7.pickle'):
+    def get_embeddings(self, input_f='data/train_only_text.pkl', output_f='train_bert_7.pkl'):
         with open(input_f, 'rb') as f:
             train = pickle.load(f)
 
