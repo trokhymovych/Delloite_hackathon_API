a021615ff90fa5e95dd64b86783201e1ddefa19d
MaksTarnavskyi <maksym.tarnavskyi@gmail.com>
2020-07-12 18:56:04 +0300

    refactor web application

--- web_application/app/routes.py
+++ web_application/app/routes.py
@@ -7,35 +7,46 @@ import json
 import shutil
 import copy
 import pickle
-
 import re
 
 from app import app
-from flask import render_template, send_from_directory
-from flask import Flask, flash, request, redirect, url_for
-from flask import jsonify
-
-import requests
-
-import urllib.request
+from flask import Flask, flash, request, redirect, url_for, Response, render_template, send_from_directory, jsonify, send_file
+from werkzeug.utils import secure_filename
 import json
-
+import urllib.request
 import urllib, json
 import requests
-
-import urllib.request
-
-from flask import send_file
-
-from werkzeug.utils import secure_filename
-
-from flask import Flask, Response, render_template, request
-import json
-
 import warnings
 warnings.filterwarnings('ignore')
 
 REQUEST_BODY = "http://0.0.0.0:8001/model/?"
+
+from app.functions_for_UI import *
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('main.html', title='Welcome', dict_for_search={}, show_evaluate_button=False, answer_dict={})
+
+    elif request.method == 'POST':
+
+        dict_for_search = {}
+        dict_for_search["accepted_function"] = request.form['accepted_function']
+        dict_for_search["rejected_function"] = request.form['rejected_function']
+        dict_for_search["accepted_product"] = request.form['accepted_product']
+        dict_for_search["rejected_product"] = request.form['rejected_product']
+        dict_for_search["company_page"] = request.form['company_page']
+
+        show_evaluate_button = check_if_valid_company_web_page(dict_for_search["company_page"])
+
+        if request.form['btn'] != "Evaluate":
+            return render_template('main.html', title='Welcome', dict_for_search=dict_for_search, show_evaluate_button=show_evaluate_button, answer_dict={})
+        else:
+            answer_dict = make_request_for_API(dict_for_search)
+        return render_template('main.html', title='Welcome', dict_for_search=dict_for_search, show_evaluate_button=show_evaluate_button, answer_dict=answer_dict)
+    else:
+        raise ValueError('Unexpected request method')
 
 
 @app.after_request
@@ -50,93 +61,6 @@ def add_header(r):
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
 
-def transform_text_for_request(text):
-    text = re.sub(r'[^\w\s]',' ',text)
-    text = re.sub(r' +','%20',text)
-    return text
-
-def make_request(dict_for_search):
-    request = copy.deepcopy(REQUEST_BODY)
-
-    for key, value in dict_for_search.items():
-        if key=="company_page":
-            request += key+"="+ value + "&"
-        else:
-            if len(value):
-                request += key+"="+ transform_text_for_request(value) + "&"
-
-    request = request[:-1]
-
-    print(request)
-    response = urllib.request.urlopen(request)
-
-    result_data = json.loads(response.read())
-
-    return result_data
-
-
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
-def index():
-    dict_for_search = {
-        'accepted_function': "",
-        'rejected_function':"",
-        'accepted_product':"",
-        'rejected_product':"",
-        'company_page':""
-    }
-
-    answer_dict = {
-        "text": "",
-        "color": "white",
-        "show_answer": False
-    }
-
-    if request.method == 'POST':
-
-
-        dict_for_search["accepted_function"] = request.form['accepted_function']
-        dict_for_search["rejected_function"] = request.form['rejected_function']
-        dict_for_search["accepted_product"] = request.form['accepted_product']
-        dict_for_search["rejected_product"] = request.form['rejected_product']
-        dict_for_search["company_page"] = request.form['company_page']
-
-        try:
-            answer_for_request = requests.get(dict_for_search["company_page"])
-            show_submit = True
-        except:
-            show_submit = False
-            flash('Incorrect web page')
-
-        if request.form['btn'] == "Evaluate":
-
-            result_data = make_request(dict_for_search)
-
-            answer_dict["text"] = result_data["Answer"]
-            if answer_dict["text"] == "Accept":
-                answer_dict["color"] = "#6aa84f"
-            else:
-                answer_dict["color"] = "#cc0000"
-            answer_dict["show_answer"] = True
-
-            return render_template('main.html', title='Welcome', dict_for_search=dict_for_search, show_submit=show_submit, answer_dict=answer_dict)
-
-        return render_template('main.html', title='Welcome', dict_for_search=dict_for_search, show_submit=show_submit,  answer_dict=answer_dict)
-
-    return render_template('main.html', title='Welcome', dict_for_search=dict_for_search, show_submit=False, answer_dict=answer_dict)
-
-
-
-
 @app.route('/robots.txt', methods=['GET', 'POST'])
 def robots():
     return send_file('robots.txt')
-
-
-#-----------------
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-def check_path(path):
-    os.system("if [ ! -d " + path + " ]; then mkdir -p " + path + "; fi")
